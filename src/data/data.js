2fa86f80626a0cdb19399c72f0ccb0dda00afd34
Bandana-Solanki24 <solankibandana24@gmail.com>
2023-08-19 15:53:27 +0530

    commit

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+// import WorkIm from "../assets/projects/workImg.jpeg";
+// import RealEst from "../assets/projects/realestate.jpg";
+import SwiggyClone from "../assets/projects/swiggy.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "React JS Application",
+    image: SwiggyClone,
+    github: "https://github.com/Bandana-Solanki24/Shopify-App",
+    live: "",
+  },
+  {
+    id: 2,
+    name: "React JS Application",
+    image: SwiggyClone,
+    github: "https://github.com/Bandana-Solanki24/swiggy-clone",
+    live: "https://swgyapp.ccbp.tech/login",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: SwiggyClone,
+    github: "",
+    live: "https://search-cake-app.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "React JS Application",
+    image: SwiggyClone,
+    github: "",
+    live: "",
+  },
+];
